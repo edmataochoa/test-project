6a789ba79a555d220549d2b9f1fdda9ac87fa25d
edmataochoa <emusicals@hotmail.com>
2017-07-10 19:27:29 -0700

    functions

--- pythontest.py
+++ pythontest.py
@@ -1,11 +1,38 @@
+class Word(object):
+    """docstring for ."""
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
+
+lexicon = {
+"house" :   ["N", "sg"],
+"to be" :   ["V"],
+"red"   :   ["ADJ", "color"]
+}
+
 def merge(a, *args):
     sequence = ""
     if args:
-        sequence += a 
+        sequence += a
         for word in args:
             sequence += " " + word
         return sequence
     else:
         return a
 
-print(merge("hey", "you", "there"))
+def get_category(word):
+    pos = lexicon.get(word)[0]
+    result = "This word is a "
+    if pos == "V":
+        return result + "verb"
+    elif pos == "N":
+        return result + "noun"
+    elif pos == "ADJ":
+        return result + "adjective"
+    else:
+        return "This word belongs to an unrecognized grammatical category"
+
+
+#print(merge("house", "to be", "red"))
+
+print(get_category("to be"))
